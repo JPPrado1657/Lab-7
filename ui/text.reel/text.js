89bdc5d6c09b54c5045a2498d59dce2726fd9f10
JPPrado1657 <noreply>
2017-08-30 03:25:40 +0000

    Add component text

---
+++ ui/text.reel/text.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/text.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Text
+ * @extends Component
+ */
+exports.Text = Component.specialize(/** @lends Text# */ {
+    constructor: {
+        value: function Text() {
+            this.super();
+        }
+    }
+});

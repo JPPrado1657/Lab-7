9101b18e2e4936091dc787e9f2e795cfda63f4e1
JPPrado1657 <noreply>
2017-08-30 02:20:34 +0000

    Add module index.html

---
+++ index.html.js
@@ -0,0 +1,15 @@
+/**
+ * @module //index.html
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class Index.html
+ * @extends Montage
+ */
+exports.Index.html = Montage.specialize(/** @lends Index.html# */ {
+    constructor: {
+        value: function Index.html() {
+            this.super();
+        }
+    }
+});
